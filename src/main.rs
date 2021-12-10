3f0aab8e72ce21af2bb2208f6a6ace88720a1ee0
Niklas Eicker <git@nikl.me>
2021-12-10 23:22:23 +0100

    Further simplify the example

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,10 @@ fn main() {
         .add_state(MyStates::Test)
         .add_system_set(
             SystemSet::on_update(MyStates::Test)
-                .with_system(first.exclusive_system().before(TestLabel::First))
-                .with_system(
-                    second
-                        .exclusive_system()
-                        .after(TestLabel::First)
-                        .before(TestLabel::Second),
-                )
-                .with_system(third.exclusive_system().after(TestLabel::Second)),
+                .with_system(first.exclusive_system().before(TestLabel::A))
+                .with_system(second.exclusive_system().after(TestLabel::A)),
         )
-        .add_system(
-            system_a
-                .exclusive_system()
-                .label(TestLabel::First)
-                .before(TestLabel::Second),
-        )
-        .add_system(system_b.exclusive_system().label(TestLabel::Second))
+        .add_system(system_a.exclusive_system().label(TestLabel::A))
         .run();
 }
 
@@ -33,22 +21,13 @@ fn second() {
     println!("2.");
 }
 
-fn third() {
-    println!("3.");
-}
-
 #[derive(SystemLabel, Eq, Debug, Hash, Clone, PartialEq)]
 pub enum TestLabel {
-    First,
-    Second,
+    A,
 }
 
 fn system_a() {
     println!("A");
-}
-
-fn system_b() {
-    println!("B");
 }
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Copy)]
